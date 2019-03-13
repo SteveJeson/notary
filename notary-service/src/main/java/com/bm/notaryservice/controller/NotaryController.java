2f722ef6528707e388c86d7fcffb33dc1c488e16
liuwei <liuwei@zench.com>
2019-03-13 16:49:55 +0800

    first commit

---
+++ notary-service/src/main/java/com/bm/notaryservice/controller/NotaryController.java
@@ -0,0 +1,13 @@
+package com.bm.notaryservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotaryController {
+
+  @RequestMapping("/hello")
+  public String index() {
+    return "Hello World!";
+  }
+}
