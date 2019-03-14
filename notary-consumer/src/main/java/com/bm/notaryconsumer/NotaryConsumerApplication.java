8c04d14f00a27061f1185866680aeb02e273435d
liuwei <liuwei@zench.com>
2019-03-14 17:34:44 +0800

    处理springboot版本兼容性问题

---
+++ notary-consumer/src/main/java/com/bm/notaryconsumer/NotaryConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.bm.notaryconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaryConsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NotaryConsumerApplication.class, args);
+  }
+
+}
--- notary-consumer/src/test/java/com/bm/notaryconsumer/NotaryConsumerApplicationTests.java
+++ notary-consumer/src/test/java/com/bm/notaryconsumer/NotaryConsumerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bm.eurekaserver;
+package com.bm.notaryconsumer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServerApplicationTests {
+public class NotaryConsumerApplicationTests {
 
   @Test
   public void contextLoads() {
