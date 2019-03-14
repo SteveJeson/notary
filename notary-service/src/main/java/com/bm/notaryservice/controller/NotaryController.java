5cf96d94f7a4c3a41d136e815bd41dd01bfe12d5
liuwei <liuwei@zench.com>
2019-03-14 15:32:15 +0800

    add logback

--- notary-service/src/main/java/com/bm/notaryservice/controller/NotaryController.java
+++ notary-service/src/main/java/com/bm/notaryservice/controller/NotaryController.java
@@ -1,13 +1,25 @@
 package com.bm.notaryservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * description 
+ * @author liuwei
+ * @return 
+ * @exception 
+ * @date 2019/3/14 15:23
+ */
 @RestController
 public class NotaryController {
 
+  private static final Logger logger = LoggerFactory.getLogger(NotaryController.class);
+
   @RequestMapping("/hello")
   public String index() {
+    logger.info("welcome to home!");
     return "Hello World!";
   }
 }
