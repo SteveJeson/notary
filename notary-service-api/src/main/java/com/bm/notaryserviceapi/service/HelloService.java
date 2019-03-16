c0f4f390ffa9d21a8152b5039c481b530aa9a05c
liuwei <SteveJasonLiuWei@163.com>
2019-03-16 16:23:25 +0800

    添加mybatis插件

--- notary-service-api/src/main/java/com/bm/notaryserviceapi/service/HelloService.java
+++ notary-service-api/src/main/java/com/bm/notaryserviceapi/service/HelloService.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @exception 
  * @date 2019/3/15 17:26
  */
-@RequestMapping("/refactor")
+//@RequestMapping("/refactor")
 public interface HelloService {
 
   @RequestMapping(value = "hello4", method = RequestMethod.GET)
